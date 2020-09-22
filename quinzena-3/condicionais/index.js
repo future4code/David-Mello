a2522f9b6376addd3297ff6adaaddc571d715cfa
David Biolchini <biolchinid@gmail.com>
2020-09-22 15:51:28 -0300

    Feitos os exercícios de interpretação

---
+++ quinzena-3/condicionais/index.js
@@ -0,0 +1,15 @@
+console.log("Oie, eu só sirvo pra saber se as coisas funcionaram!");
+// Exercícios de interpretação 
+// EX 01:
+// O código testa se o numero dado é par ou impar, se par ele mostra no console "Passou no teste",
+// se impar "Não passou no teste".
+
+// EX 02:
+// a. O código serve para mostrar o preço da fruta escolhida pelo usuário.
+// b. "O preço da fruta Maçã é R$ 2.25 ".
+// c. O código imprime o preço definido em "default".
+
+// EX O3
+// a. A primeira linha pede que o usuário digite um número, transforma a string digitada em número
+//  e armazena na variável "numero".
+// b. 10 - "Esse número passou no teste", -10 - não há mensagem.
