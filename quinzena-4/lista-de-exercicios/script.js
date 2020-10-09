7057b6957a92ff6a9916e55af8da836644ca6493
David Biolchini <biolchinid@gmail.com>
2020-10-09 19:54:04 -0300

    fifth commit

--- quinzena-4/lista-de-exercicios/script.js
+++ quinzena-4/lista-de-exercicios/script.js
@@ -258,3 +258,31 @@ const arrayString = (array) => {
 
 
 //03
+const pessoas = [
+	{ nome: "Paula", idade: 12, altura: 1.8},
+	{ nome: "João", idade: 20, altura: 1.3},
+	{ nome: "Pedro", idade: 15, altura: 1.9},
+	{ nome: "Luciano", idade: 22, altura: 1.8},
+	{ nome: "Artur", idade: 10, altura: 1.2},
+	{ nome: "Soter", idade: 70, altura: 1.9}
+]
+
+// a)
+
+const pessoasPermitidas= (array) => {
+    return array.filter(element => {
+        return element.idade > 14 && element.idade < 60 && element.altura >= 1.5;
+    });
+}
+
+//b)
+
+const pessoasNaoPermitidas = (array) => {
+    return array.filter( element => {
+        return element.idade < 14 || element.idade > 60 || element.altura < 1.5;
+    });
+}
+
+//04
+
+ 
